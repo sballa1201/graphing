7f6dedd5fb6cea15d9eadf3c56c63734dda5b52e
Shashidhara <Shashidhara@SriRama2-HP.lan>
2018-08-08 14:33:26 +0100

    implementing the basics of the tree class, its attributes + getter and setter for the attribute value

---
+++ Graphing/src/structures/Tree.java
@@ -0,0 +1,24 @@
+package structures;
+
+public class Tree {
+	
+	public Tree left;
+	public Tree right;
+	private String value;
+	
+	public Tree() {
+		this.left = null;
+		this.right = null;
+		
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	
+}
