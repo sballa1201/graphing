3578972c48df00ddf8810d541c206abe8506fd19
Shashidhara <Shashidhara@SriRama2-HP.lan>
2018-08-08 16:34:31 +0100

    implemented the barebones of function class, functions not done yet

---
+++ Graphing/src/structures/Function.java
@@ -0,0 +1,19 @@
+package structures;
+
+public class Function {
+	
+	private Stack<String> postFixStack;
+	
+	public Function() {
+		
+	}
+	
+	public double evaluate(double x) {
+		
+	}
+	
+	private Stack<String> substitute(double x) {
+		
+	}
+
+}
