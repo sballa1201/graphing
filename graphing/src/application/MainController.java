39ca1756af147808aec4a888fc908e88309d1fa9
Shashi Balla HP Laptop <ballashashi@gmail.com>
2018-08-13 16:49:00 +0100

    Made the program more modular  by splitting the plot into its own separate class

--- graphing/src/application/MainController.java
+++ graphing/src/application/MainController.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -9,9 +10,11 @@ import exceptions.StackUnderflowException;
 import exceptions.UnequalBracketsException;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import structures.Function;
@@ -19,13 +22,13 @@ import structures.Function;
 public class MainController implements Initializable {
 	
 	@FXML
-	private Pane canvasPane;
+	private BorderPane rootPane;
 	
 	@FXML
 	private Canvas canvas;
 	
 	private GraphicsContext gc;
-	private ArrayList<Function> functions = new ArrayList<Function>();
+	public static ArrayList<Function> functions = new ArrayList<Function>();
 	
 	private int steps = 1000;
 	private double minX = -10;
@@ -38,6 +41,7 @@ public class MainController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		/*
 		gc = canvas.getGraphicsContext2D();
 		canvasPane.setStyle("-fx-background-color: white");
 		canvas.heightProperty().bind(canvasPane.heightProperty());
@@ -61,79 +65,16 @@ public class MainController implements Initializable {
 		};
 		canvasPane.heightProperty().addListener(redrawListener);
 		canvasPane.widthProperty().addListener(redrawListener);
-		
-	}
-	
-	@FXML
-	private void draw() throws StackOverflowException, StackUnderflowException, UnequalBracketsException { 
-		this.updatePixelWorth();
-		gc.clearRect(0, 0,canvas.getWidth(), canvas.getHeight());
-		
-		for(Function f:this.functions) {
-			this.drawFunction(f);
+		*/
+		Pane plotPane = null;
+		try {
+			plotPane = FXMLLoader.load(getClass().getResource("/PlotPane.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		
-	}
-	
-	private void drawFunction(Function f, Color color) throws StackUnderflowException, StackOverflowException {
-		gc.setLineWidth(1.5);
-		gc.setStroke(color);
-		double x1, x2, y1, y2;
-		x1 = this.minX;
-		y1 = f.evaluate(x1);
-		
-		double step = (this.maxX - this.minX)/this.steps;
-
-		for(x2=this.minX+step; x2<this.maxX;x2=x2+step) {
-			try {
-				y2 = f.evaluate(x2);
-				gc.strokeLine(this.convertX(x1), this.convertY(y1), this.convertX(x2), this.convertY(y2));
-				x1 = x2;
-				y1 = y2;
-			} catch(ArithmeticException e) {
-				x1 = x2 + step;
-				x2 = x1;
-				y1 = f.evaluate(x1);
-			}
-			
-		}
-		
-		x2 = this.maxX;
-		y2 = f.evaluate(x2);
-		gc.strokeLine(this.convertX(x1), this.convertY(y1), this.convertX(x2), this.convertY(y2));
-	}
-	
-	private void drawFunction(Function f) throws StackUnderflowException, StackOverflowException {
-		this.drawFunction(f, Color.BLACK);
-	}
-	
-	//TODO
-	private void drawAsymptote(double x) {
-
-	}
-	
-	//TODO
-	private void drawAxes() {
-		
-	}
-	
-	private void updatePixelWorth() {
-		this.pixelWorthX = Math.abs((this.maxX - this.minX)/canvas.getWidth());
-		this.pixelWorthY = Math.abs((this.maxY - this.minY)/canvas.getHeight());
-		
-	}
-	
-	private double convertX(double x) {
-		x = x - this.minX;
-		x = x /this.pixelWorthX;
-		return x;
-	}
-	
-	private double convertY(double y) {
-		y = this.maxY - y;
-		y = y/this.pixelWorthY;
-		return y;
+		rootPane.setCenter(plotPane);
 	}
 	
 	
---
+++ graphing/src/application/PlotPaneController.java
@@ -0,0 +1,142 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import exceptions.StackOverflowException;
+import exceptions.StackUnderflowException;
+import exceptions.UnequalBracketsException;
+import javafx.beans.value.ChangeListener;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import structures.Function;
+
+public class PlotPaneController implements Initializable {
+	
+	
+	@FXML
+	private Pane plotPane;
+	
+	@FXML
+	private Canvas canvas;
+	
+	private GraphicsContext gc;
+	
+	
+	private int steps = 1000;
+	private double minX = -10;
+	private double maxX = 10;
+	private double minY = -10;
+	private double maxY = 10;
+	private double pixelWorthX;
+	private double pixelWorthY;
+	
+	public PlotPaneController() {
+		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		gc = canvas.getGraphicsContext2D();
+		plotPane.setStyle("-fx-background-color: white");
+		canvas.heightProperty().bind(plotPane.heightProperty());
+		canvas.widthProperty().bind(plotPane.widthProperty());
+		try {
+			//Function f = new Function("x^2");
+			Function g = new Function("1/x + 1/(x+2)");
+			//this.functions.add(f);
+			MainController.functions.add(g);
+			
+			
+		} catch (StackOverflowException | StackUnderflowException | UnequalBracketsException e) {
+			e.printStackTrace();
+		}
+		ChangeListener<Number> redrawListener = (observable, oldValue, newValue) -> {
+			try {
+				draw();
+			} catch (StackOverflowException | StackUnderflowException | UnequalBracketsException e) {
+				e.printStackTrace();
+			}
+		};
+		plotPane.heightProperty().addListener(redrawListener);
+		plotPane.widthProperty().addListener(redrawListener);
+
+	}
+	
+	@FXML
+	private void draw() throws StackOverflowException, StackUnderflowException, UnequalBracketsException { 
+		this.updatePixelWorth();
+		gc.clearRect(0, 0,canvas.getWidth(), canvas.getHeight());
+		
+		for(Function f:MainController.functions) {
+			this.drawFunction(f);
+		}
+		
+		
+	}
+	
+	private void drawFunction(Function f, Color color) throws StackUnderflowException, StackOverflowException {
+		gc.setLineWidth(1.5);
+		gc.setStroke(color);
+		double x1, x2, y1, y2;
+		x1 = this.minX;
+		y1 = f.evaluate(x1);
+		
+		double step = (this.maxX - this.minX)/this.steps;
+
+		for(x2=this.minX+step; x2<this.maxX;x2=x2+step) {
+			try {
+				y2 = f.evaluate(x2);
+				gc.strokeLine(this.convertX(x1), this.convertY(y1), this.convertX(x2), this.convertY(y2));
+				x1 = x2;
+				y1 = y2;
+			} catch(ArithmeticException e) {
+				x1 = x2 + step;
+				x2 = x1;
+				y1 = f.evaluate(x1);
+			}
+			
+		}
+		
+		x2 = this.maxX;
+		y2 = f.evaluate(x2);
+		gc.strokeLine(this.convertX(x1), this.convertY(y1), this.convertX(x2), this.convertY(y2));
+	}
+	
+	private void drawFunction(Function f) throws StackUnderflowException, StackOverflowException {
+		this.drawFunction(f, Color.BLACK);
+	}
+	
+	//TODO
+	private void drawAsymptote(double x) {
+
+	}
+	
+	//TODO
+	private void drawAxes() {
+		
+	}
+	
+	private void updatePixelWorth() {
+		this.pixelWorthX = Math.abs((this.maxX - this.minX)/canvas.getWidth());
+		this.pixelWorthY = Math.abs((this.maxY - this.minY)/canvas.getHeight());
+		
+	}
+	
+	private double convertX(double x) {
+		x = x - this.minX;
+		x = x /this.pixelWorthX;
+		return x;
+	}
+	
+	private double convertY(double y) {
+		y = this.maxY - y;
+		y = y/this.pixelWorthY;
+		return y;
+	}
+
+}
